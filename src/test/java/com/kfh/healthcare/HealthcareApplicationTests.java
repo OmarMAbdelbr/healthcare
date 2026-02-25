89dc9aef3732b6ec6842e7fd7de1926daf6d997c
OmarMAbdelbr <omar.magdy.abd.elbar@gmail.com>
2026-02-25 02:38:01 +0200

    initial commit

---
+++ src/test/java/com/kfh/healthcare/HealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfh.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
